1bf4903f582dc5dc5a460334d9188f468bf21a72
Sumit Bhowmick <sumitbhowmick@gmail.com>
2025-04-05 10:45:16 +0530

    comments

--- hello.js
+++ hello.js
@@ -1,11 +1,13 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+//default options
 export const options = {
   vus: 10,
   duration: "30s",
 };
 
+//function
 export default function () {
   http.get("https://test.k6.io/");
   sleep(1);
